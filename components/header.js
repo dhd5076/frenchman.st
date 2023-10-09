99976a69e4ffe0c1fd36c84f6e21856e7c81ed8d
Dylan Dunn <dylandunn17@icloud.com>
2023-10-09 14:54:06 -0400

    Added location and contact info

--- components/header.js
+++ components/header.js
@@ -31,32 +31,36 @@ const Navbar = () => {
         })
     });
 
-  return (
-    <nav className="fixed top-0 left-0 w-full bg-accent/90 backdrop-blur text-white z-50 shadow-xl shadow-black/25">
-      <div className="container mx-auto flex rosewood-font justify-between items-center py-4">
-        <Link href="/" className="text-3xl pl-6">
-          Frenchman Street
-        </Link>
-        <div className="hidden md:flex space-x-6 text-lg">
-          <Link href="/#top" className="hover:text-white/50 transition duration-300 ease-in-out">
-            Home
+    return (
+      <nav className="fixed top-0 left-0 w-full bg-accent/90 backdrop-blur text-white z-50 shadow-xl shadow-black/25 px-6">
+        <div className="container mx-auto flex rosewood-font justify-between py-2 items-center"> {/* Add 'items-center' */}
+          <Link href="/" className="text-2xl align-baseline inline-block pt-2">
+              Frenchman Street
           </Link>
-          <Link href="/#first" className="hover:text-white/50 t transition duration-300 ease-in-out">
-            Menu
+          <div className="hidden md:flex space-x-6 text-lg">
+            <Link href="/#top" className="hover:text-white/50 transition duration-300 ease-in-out">
+              Home
+            </Link>
+            <Link href="/#first" className="hover:text-white/50 transition duration-300 ease-in-out">
+              Menu
+            </Link>
+            <a href="/events" className="hover:text-white/50 transition duration-300 ease-in-out">
+              About
+            </a>
+            <a href="/contact" className="hover:text-white/50 transition duration-300 ease-in-out">
+              Contact
+            </a>
+            <a href="/contact" className="hover:text-white/50 transition duration-300 ease-in-out">
+              Order Online
+            </a>
+          </div>
+          <Link href="/" className="fa fa-bars text-2xl md:hidden">
+
           </Link>
-          <a href="/events" className="hover:text-white/50  transition duration-300 ease-in-out">
-            Our Story
-          </a>
-          <a href="/contact" className="hover:text-white/50  transition duration-300 ease-in-out">
-            Get In Touch
-          </a>
         </div>
-        <div className="md:hidden">
-          <MenuAlt1 className="h-8 w-8 cursor-pointer mr-6" />
-        </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+    
 };
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -47,12 +47,12 @@ export default function Home() {
             />
         </div>
         <div className="h-auto w-full" id="top">
-            <div className={`transition-all duration-1000 ${isVisible ? 'bg-black/75' : 'bg-black/100'}`}>
+            <div className={`transition-all duration-1000 ${isVisible ? 'bg-black/50' : 'bg-black/100'}`}>
             <div className="relative h-screen">
-                <img src="/logo/Frenchman Street Alternate Logo.svg" className={`w-full h-full md:object-cover object-center transition-all delay-500 duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`} alt="Background" />
+                <img src="/logo/Frenchman Street Alternate Logo.svg" className={`w-full mx-auto h-full md:object-cover object-center transition-all delay-500 duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`} alt="Background" />
                 <div className="absolute inset-0 flex flex-col justify-end items-center">
                     <div className="text-white mb-8">
-                    <span className="fas fa-chevron-down shadow-xl opacity-50 hover:opacity-75 text-5xl pb-16 md:pb-0 animate-bounce hover:cursor-pointer" onClick={() => { scrollToElementById("first"); setShowScroll(false) }}></span>
+                    <span className="fas fa-chevron-down opacity-50 hover:opacity-75 text-5xl pb-16 md:pb-0 animate-bounce hover:cursor-pointer" onClick={() => { scrollToElementById("first"); setShowScroll(false) }}></span>
                     </div>
                 </div>
             </div>
@@ -60,17 +60,43 @@ export default function Home() {
             </div>
             <FadeElement before='' className="md:flex bg-primary min-h-[100vh]" id="first">
                 <div className="md:w-1/2 bg-black">
-                    <FadeElement>
+                    <FadeElement before="opacity-0" after="opacity-100">
                         <img src="place.jpg" className="w-full h-full object-cover"/>
                     </FadeElement>
                 </div>
-                <div className="md:w-1/2 p-8 rosewood-font flex flex-col items-center justify-center text-left">
+                <div className="md:w-1/2 p-6 pb-0 rosewood-font flex flex-col items-center justify-center text-left">
                     <div className="w-full">
-                        <FadeElement>
-                            <div className="border-b-4 pb-6 max-w-lg mr-auto">
-                                <h2 className="text-3xl text-white mb-2">Welcome to the Taste of New Orleans</h2>
+                        <FadeElement before="opacity-0" after="opacity-100">
+                            <div className="pb-6 max-w-lg mr-auto">
+                                <h2 className="text-3xl text-white mb-2">Welcome To New Orleans</h2>
                                 <p className="text-white">Indulge in the vibrant flavors of the Big Easy right here in the heart of Rochester. Our New Orleans-inspired restaurant brings you a taste of Louisiana&apos;s rich culinary heritage with a menu bursting with Cajun and Creole classics. From gumbo to jambalaya, our dishes are crafted with authentic spices and local ingredients, guaranteeing a soulful dining experience that transports you straight to the streets of New Orleans. Join us and savor the magic of the French Quarter in every bite.</p>
-                                <button className="my-4 hover:scale-[1.1] shadow-xl transition-all duration-100 bg-secondary text-black block px-4 py-4 mr-auto" onClick={() => scrollToElementById("second")}> Learn More </button>
+                                <button className=" mt-6 hover:scale-[1.1] transition-all duration-100 bg-secondary text-black block px-4 py-2 font-serif font-bold mr-auto" onClick={() => scrollToElementById("second")}> Learn More </button>
+                            </div>
+                            <div className="text-accent p-6 -mx-6 space-y-4 bg-white">
+                                <div className="flex">
+                                    <span className="fas fa-map-pin w-8"/>
+                                    <div className="font-sans">
+                                        <b>Mercantile On Main</b> <br/>
+                                        240 E. Main St. <br/>
+                                        Rochester, NY 14603
+
+                                    </div>
+                                </div>
+                                <div className="flex">
+                                    <span className="fas fa-clock w-8"/>
+                                    <div className="font-sans">
+                                        <b>Monday - Friday</b> <br/>
+                                        11:30am - 8:00pm <br/>
+                                        <b>Saturday - Sunday</b> <br/>
+                                        <span className="indent-4">Private Events Only</span>
+                                    </div>
+                                </div>
+                                <div>
+                                        <span className="fas fa-phone w-8"/><span className="font-sans font-bold">(585) 555-1234</span>
+                                    </div>
+                                    <div className="">
+                                        <span className="fas fa-envelope w-8"/><span className="font-sans font-bold">contact@frenchman.st</span>
+                                    </div>
                             </div>
                         </FadeElement>
                     </div>
@@ -82,19 +108,19 @@ export default function Home() {
                         <img src="chef.jpeg" className="w-full h-full object-cover"/>
                     </FadeElement>
                 </div>
-                <FadeElement className="md:w-1/2 p-8 rosewood-font flex flex-col items-center justify-center text-right">
+                <FadeElement className="md:w-1/2 p-6 rosewood-font flex flex-col items-center justify-center text-right">
                     <div className="w-full">
                         <FadeElement before="opacity-0" after="opacity-100">
-                            <div className="border-b-4 border-accent pb-6 max-w-lg ml-auto">
+                            <div className="max-w-lg ml-auto">
                                 <h2 className="text-3xl mb-2">Savor the Culinary Magic of New Orleans: Meet Our Renowned Chef</h2>
                                 <p className="">Explore the rich flavors and vibrant culture of New Orleans through the exquisite creations of our master chef. Get to know the culinary artist behind the enchanting dishes that capture the essence of the Crescent City.</p>
-                                <button className="my-4 bg-primary hover:scale-[1.1] shadow-xl transition text-white block px-4 py-4 ml-auto" onClick={() => scrollToElementById("menu")}> View Menu </button>
+                                <button className=" mt-4  hover:scale-[1.1] transition-all duration-100 bg-accent text-white block px-4 py-2 font-serif font-bold ml-auto" onClick={() => scrollToElementById("menu")}> View Menu </button>
                             </div>
                         </FadeElement>
                     </div>
                 </FadeElement>
             </div>
-            <div className="p-6 font-sans bg-black text-white rosewood-font" id="menu">
+            <div className="px-6 pb-6 font-sans bg-black text-white rosewood-font min-h-[100vh]" id="menu">
                 <FadeElement before='opacity-0 scale-[0.9]' after='opacity-100 scale-1 h-full'>
                     <Slider 
                     slidesToShow={3} 
@@ -111,7 +137,7 @@ export default function Home() {
                           },
                         },
                       ]} 
-                    className="bg-black -mx-6 mt-6" 
+                    className="bg-black -mx-6" 
                     infinite={true}>
                         <img src="/food.jpg" alt="Image 1" className="h-full"/>
                         <img src="/bg.jpeg"  alt="Image 1" className="h-full"/>
@@ -120,11 +146,8 @@ export default function Home() {
                     </Slider>
                 </FadeElement>
                 <span className="items-end justify-start b-2 border-b-2">
-                    <div className="flex justify-between">
-                        <h2 className="text-6xl mt-10 mx-6"> MENU </h2>
-                        <button className="my-auto bg-highlight/90 inline-block mt-10  p-4 text-lg text-black shadow-xl transition-all duration-300 hover:scale-[1.1]"> Order Online</button>
-                    </div>
-                    <div className="sm:flex ml-6 sm:space-x-6 mx-6 mb-10 text-lg mt-4">
+                    <h2 className="text-6xl mt-10"> MENU </h2>
+                    <div className="sm:flex sm:space-x-6 space-y-4 sm:space-y-0 mb-10 text-lg mt-4">
                         <h3 className="hover:border-white transition-all duration-300 border-white/25 border-b-2 hover:cursor-pointer"> Appetizers</h3>
                         <h3 className="hover:border-white transition-all duration-300 border-white/25 border-b-2 hover:cursor-pointer"> Mains</h3>
                         <h3 className="hover:border-white transition-all duration-300 border-white/25 border-b-2 hover:cursor-pointer"> Sides</h3>
@@ -132,7 +155,7 @@ export default function Home() {
                         <h3 className="hover:border-white transition-all duration-300 border-white/25 border-b-2 hover:cursor-pointer"> Desserts</h3>
                     </div>
                 </span>
-                <div className="md:flex flex-wrap grid-cols-3 md:space-x-6 pb-6 px-6">
+                <div className="md:flex flex-wrap grid-cols-3 md:space-x-6">
                     <div className="md:w-1/4 font-semibold mt-6 md:mt-0">
                         <FadeElement before='opacity-0' after='opacity-100'>
                         <div>
@@ -148,14 +171,14 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="backdrop-blur bg-gradient-to-r p-6">
-                <div className="max-w-xl mx-auto p-6 border-2 bg-black/50">
-                    <a className="block text-center mx-auto" href="https://avvinorochester.com">
+            <div className="backdrop-blur bg-gradient-to-r bg-white p-6">
+                <div className="max-w-xl mx-auto">
+                    <a className="block max-w-sm text-center mx-auto filter invert" href="https://avvinorochester.com">
                         <Image width={1024} height={1024} alt="Logo For Avvino, Text surrounded by curly frills" src="/avvino.png" className="mb-4 px-4 py-2 rounded object-fit" />
                     </a>
-                    <div className=" mx-auto">
-                        <h2 className='font-bold text-lg text-white font-serif underline mb-2 mt-0 text-center'> From Avvino To Frenchman Street</h2>
-                        <p className="text-white mx-6 mb-6 text-sm font-serif mt-2 text-center">
+                    <div className="mx-auto">
+                        <h2 className='font-bold text-lg text-black font-serif mb-2 mt-0 text-center'> From Avvino To Frenchman Street</h2>
+                        <p className="text-black text-sm font-serif mt-2 text-center">
                             Frenchman Street is a proud offspring of Avvino, our original and highly acclaimed restaurant. Avvino, renowned for its exceptional dining experience and rich culinary heritage, has been a cornerstone of our community for many years. As we continue to evolve and innovate, we decided to introduce La Bola, a new venture that carries forward Avvino&apos;s legacy while embracing the vibrant flavors of Spain and the Mediterranean. La Bola promises to enchant your taste buds with an array of delightful dishes, inspired by the same passion and expertise that originated at Avvino.
                         </p>
                     </div>
@@ -163,8 +186,8 @@ export default function Home() {
             </div>
 
             <div className="w-full p-4 text-center text-white bg-black">
-                <span className="text-center w-full block">Copyright &copy; 2023 Frenchman Street</span>
-                <span className="text-xs"> Made With ❤️ By <a className="underline" href="https://dylandunn.me">Dylan Dunn</a></span>
+                <span className="text-center w-full block rosewood-font">Copyright &copy; 2023 Frenchman Street</span>
+                <span className="text-xs italic"> Made With ❤️ By <a className="underline" href="https://dylandunn.me">Dylan Dunn</a></span>
             </div>
         </div>
     </>
