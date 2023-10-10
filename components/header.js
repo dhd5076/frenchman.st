73f1c240991fc8497b0c3d591cd77f756bdb1ec3
Dylan Dunn <dylandunn17@icloud.com>
2023-10-09 22:39:19 -0400

    Mobile Dropdown Menu

--- components/header.js
+++ components/header.js
@@ -5,9 +5,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
-
-  const [showScroll, setShowScroll] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
     const scrollToElementById = (elementId) => {
         const element = document.getElementById(elementId);
@@ -17,49 +15,61 @@ const Navbar = () => {
         }
     };
 
+    const toggleShowMenu = () => {
+      setShowMenu(!showMenu);
+    }
+
     useEffect(() => {
 
-        setIsVisible(true);
-
-        window.addEventListener('scroll', () => {
-            if(window.scrollY > 300) {
-                setShowScroll(true);
-            } else {
-                setShowScroll(false);
-            }
-
-        })
     });
 
     return (
-      <nav className="fixed top-0 left-0 w-full bg-accent/90 backdrop-blur text-white z-50 shadow-xl shadow-black/25 px-6">
-        <div className="container mx-auto flex rosewood-font justify-between py-2 items-center"> {/* Add 'items-center' */}
-          <Link href="/" className="text-2xl align-baseline inline-block pt-2">
-              Frenchman Street
-          </Link>
-          <div className="hidden md:flex space-x-6 text-lg">
-            <Link href="/#top" className="hover:text-white/50 transition duration-300 ease-in-out">
-              Home
-            </Link>
-            <Link href="/#first" className="hover:text-white/50 transition duration-300 ease-in-out">
-              Menu
-            </Link>
-            <a href="/events" className="hover:text-white/50 transition duration-300 ease-in-out">
-              About
-            </a>
-            <a href="/contact" className="hover:text-white/50 transition duration-300 ease-in-out">
-              Contact
-            </a>
-            <a href="/contact" className="hover:text-white/50 transition duration-300 ease-in-out">
-              Order Online
-            </a>
+      <>
+        <nav className="fixed top-0 left-0 w-full bg-accent/90 text-white z-50 shadow-xl shadow-black/25">
+          <div className="rosewood-font container text-xl -my-2 mx-auto flex justify-between px-6">
+            <img src="/barcode.png" className="-my-auto h-16 my-4"/>
+            <div className="hidden md:flex space-x-6 ml-auto my-auto">
+              <Link href="/#top" className="hover:text-white/50 transition duration-300 ease-in-out">
+                Home
+              </Link>
+              <Link href="/#first" className="hover:text-white/50 transition duration-300 ease-in-out">
+                Menu
+              </Link>
+              <a href="/events" className="hover:text-white/50 transition duration-300 ease-in-out">
+                About
+              </a>
+              <a href="/contact" className="hover:text-white/50 transition duration-300 ease-in-out">
+                Contact
+              </a>
+              <a href="/contact" className="hover:text-white/50 transition duration-300 ease-in-out">
+                Order Online
+              </a>
+            </div>
+            <button href="/" className="fa fa-bars align-top text-3xl md:hidden my-auto" onClick={toggleShowMenu}></button>
           </div>
-          <Link href="/" className="fa fa-bars text-2xl md:hidden">
-
-          </Link>
-        </div>
-      </nav>
+          <div className={`w-full overflow-hidden px-6 ${showMenu ? 'h-[100vh] bg-black/75 visible opacity-100 backdrop-blur' : 'invisible h-0 opacity-0'} duration-1000 transition-all`}>
+            <div className="space-y-8 ml-auto my-auto pt-16 rosewood-font">
+              <Link href="/#top" className="block hover:text-white/50 transition duration-300 ease-in-out text-center">
+                Home
+              </Link>
+              <Link href="/#first" className="block hover:text-white/50 transition duration-300 ease-in-out text-center">
+                Menu
+              </Link>
+              <a href="/events" className="block hover:text-white/50 transition duration-300 ease-in-out text-center">
+                About
+              </a>
+              <a href="/contact" className="block hover:text-white/50 transition duration-300 ease-in-out text-center">
+                Contact
+              </a>
+              <a href="/contact" className="block hover:text-white/50 transition duration-300 ease-in-out text-center">
+                Order Online
+              </a>
+            </div>
+          </div>
+        </nav>
+      </>
     );
+    
     
 };
 
